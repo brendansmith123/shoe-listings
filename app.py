7355f9550fac7a2ca8fe0a05a70d7284dd7e630a
brendansmith123 <92564077+brendansmith123@users.noreply.github.com>
2021-12-09 19:53:19 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
- 
+import os
 from pymongo import MongoClient
-client = MongoClient()
+uri = os.environ.get('MONGODB_URI', 'mongodb+srv://metrobrendan:Amazing7@Cluster0.cbl9h.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient(uri)
 db = client.shoelist
 app = Flask(__name__)
 
